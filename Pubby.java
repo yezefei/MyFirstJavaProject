92149bf0fb4e1f0be85c5e59e8630ab53efbbdc0
✎﹏ℳ泽飞  <maofeifei>
2018-11-05 06:58:03 +0000

    版本第一次提交

---
+++ Pubby.java
@@ -0,0 +1,10 @@
+
+public class Pubby{
+    public Pubby(String name){
+        System.out.println("小狗的名字是:"+name);
+    }
+
+    public static void main(String []args){
+        Pubby pubby = new Pubby("jermeyLin");
+    }
+}
